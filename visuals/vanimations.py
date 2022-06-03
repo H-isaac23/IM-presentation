8dde3384491b459d83bac07907498c3bd9fb9134
Holo-23 <dev.isaac23@gmail.com>
2022-06-03 08:47:50 +0800

    create custom animation count

---
+++ visuals/vanimations.py
@@ -0,0 +1,11 @@
+from manim import *
+
+class Count(Animation):
+    def __init__(self, number: DecimalNumber, start: float, end: float, **kwargs) -> None:
+        super().__init__(number,  **kwargs)
+        self.start = start
+        self.end = end
+
+    def interpolate_mobject(self, alpha: float) -> None:
+        value = self.start + (alpha * (self.end - self.start))
+        self.mobject.set_value(value)
